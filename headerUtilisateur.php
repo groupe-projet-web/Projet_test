185299ce15797f567a0bf94cf320ac19bbca89b0
Edwige MONTHE DEUTCHUI <monthee@3il.fr>
2020-01-24 17:37:35 +0100

    Ajout des fichiers de Edwige

---
+++ headerUtilisateur.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+        <link rel="stylesheet" type = "text/css" href="/miniprojetweb/style.css" />
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+</head>
+<body>
+	<div style="background-color:#E6E6FA">
+    
+		<nav class="main-navigation">
+			<div class="navbar-header animated fadeInUp">
+			</div>
+			<ul class="nav-list">
+				<li class="nav-list-item">
+					<a href="#" class="nav-link">Home</a>
+				</li>
+				<li class="nav-list-item">
+					<a href="ouverturedecompte.html" class="nav-link">Creer un Compte</a>
+				</li>
+				<li class="nav-list-item">
+					<a href="deconnexion.php" class="nav-link">Deconnexion</a>
+				</li>
+				<li class="nav-list-item">
+				</li>
+			</ul>
+
+		</nav>
+
+	</div>
+</body>
+</html>
