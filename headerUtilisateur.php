9cf052145dddf42432257ba092ba4fe55ff71835
Edwige MONTHE DEUTCHUI <monthee@3il.fr>
2020-01-29 08:53:39 +0100

    le footer et le header ont changer

--- headerUtilisateur.php
+++ headerUtilisateur.php
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
     <meta charset="utf-8"/>
-        <link rel="stylesheet" type = "text/css" href="/miniprojetweb/style.css" />
+	<link rel="stylesheet" type = "text/css" href="/miniprojetweb/style.css" />
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!------ Include the above in your HEAD tag ---------->
-</head>
-<body>
+
+<header>
 	<div style="background-color:#E6E6FA">
     
 		<nav class="main-navigation">
 			<div class="navbar-header animated fadeInUp">
 			</div>
 			<ul class="nav-list">
-				<li class="nav-list-item">
-					<a href="#" class="nav-link">Home New</a>
-				</li>
 				<li class="nav-list-item">
 					<a href="ouverturedecompte.html" class="nav-link">Creer un Compte</a>
 				</li>
@@ -31,5 +25,4 @@
 		</nav>
 
 	</div>
-</body>
-</html>
+</header>
